bde466d067f7434fad205ec0066e4f55cf596b19
Mogan Xumerle <moganrz@gmail.com>
2022-02-09 23:18:54 -0300

    feat: create basic CLI commands

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/moganxumerle/study-go-todolist
+
+go 1.16
+
+require github.com/spf13/cobra v1.3.0 // indirect
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/moganxumerle/study-go-todolist/cmd"
+
+func main() {
+	cmd.RootCmd.Execute()
+}

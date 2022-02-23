65f27108e5d37008ec349882f2817a904fab2b2d
Mogan Xumerle <moganrz@gmail.com>
2022-02-22 22:51:37 -0300

    feat: connection on bolt DB and create new bucket

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/moganxumerle/study-go-todolist
 
 go 1.16
 
-require github.com/spf13/cobra v1.3.0 // indirect
+require (
+	github.com/boltdb/bolt v1.3.1
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/cobra v1.3.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/moganxumerle/study-go-todolist/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/moganxumerle/study-go-todolist/cmd"
+	"github.com/moganxumerle/study-go-todolist/db"
+)
 
 func main() {
-	cmd.RootCmd.Execute()
+	home, _ := homedir.Dir()
+	fmt.Println(home)
+	dbPath := filepath.Join(home, "task.db")
+	must(db.Init(dbPath))
+	must(cmd.RootCmd.Execute())
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }

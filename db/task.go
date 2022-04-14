2cf8d09528dc8bc20b0761fb4f9b27770f88a4e5
Mogan Xumerle <moganrz@gmail.com>
2022-04-14 08:42:27 -0300

    feat: delete task boltdb

--- db/task.go
+++ db/task.go
@@ -69,6 +69,13 @@ func ListAllTasks() ([]Task, error) {
 	return tasks, nil
 }
 
+func DeleteTask(key int) error {
+	return db.Update(func(t *bolt.Tx) error {
+		b := t.Bucket(taskBucket)
+		return b.Delete(itob(key))
+	})
+}
+
 func itob(v int) []byte {
 	b := make([]byte, 8)
 	binary.BigEndian.PutUint64(b, uint64(v))
